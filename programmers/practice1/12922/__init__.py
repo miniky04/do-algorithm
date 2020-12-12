2e8990c41964918f84f1a45dba0cc5f19dfab6c8
minik <miniky04@naver.com>
2020-12-12 19:37:55 +0900

    12922 풀었음

---
+++ programmers/practice1/12922/__init__.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = ''
+    for i in range(n):
+        if i % 2 == 0:
+            answer += '수'
+        elif i % 2 == 1:
+            answer += '박'
+    return answer
+
+
+n2 = 3
+print(solution(n2))

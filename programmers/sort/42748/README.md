7e1d10cccce8cd475621d2415269dfd2789984d9
minik <miniky04@naver.com>
2020-12-13 21:21:20 +0900

    42748 간단 수정

--- programmers/sort/42748/README.md
+++ programmers/sort/42748/README.md
@@ -3,8 +3,8 @@ https://programmers.co.kr/learn/courses/30/lessons/42748
 
 설명 : 
 1. 변수설명 :
-   array2 = 받아온 배열 변수
-   commands2 = [[i,j,k]] -> 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을때, k번째에 있는 수를 구한다.
+   array3 = 받아온 배열 변수
+   command = [[i,j,k]] -> 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을때, k번째에 있는 수를 구한다.
    answer = 빈리스트
 2. 코드설명 :
 commands값을 command에 하나씩 담는다
--- programmers/sort/42748/__init__.py
+++ programmers/sort/42748/__init__.py
@@ -1,14 +1,12 @@
-array2 = [1, 5, 2, 6, 3, 7, 4]
-commands2 = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
-
-
 def solution(array, commands):
     answer = []
-    for command in commands: #command = [2,5,3]
+    for command in commands: #command = [4,4,1]
         array3 = array[command[0]-1:command[1]] #array3 = [5,2,6,3]
         array3.sort() #array3 = [2,3,5,6]
         answer.append(array3[command[2]-1])
     return answer
 
 
+array2 = [1, 5, 2, 6, 3, 7, 4]
+commands2 = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 print(solution(array2, commands2))

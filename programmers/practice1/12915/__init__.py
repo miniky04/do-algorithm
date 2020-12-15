0747d55d8f99b32e7ddbfff08ea99ba35eb1316f
minik <miniky04@naver.com>
2020-12-15 22:52:19 +0900

    12915 다시 풀기

---
+++ programmers/practice1/12915/__init__.py
@@ -0,0 +1,31 @@
+# def solution(strings, n):
+#     answer = []
+#     l = []
+#     l2 = []
+#     # 2차원 배열로 만들어주는 것 [['sun'], ['bed'], ['car']]
+#     # answer = [[x] for x in strings]
+#     # [['s', 'u', 'n'], ['b', 'e', 'd'], ['c', 'a', 'r']]
+#     for i in strings:
+#         l.append(list(i))
+#     # ['a', 'e', 'u']
+#     for i in range(len(strings)):
+#         l2.append(l[i][n])
+#         l2.sort()
+#     for i in range(len(l2)):  # 0--2
+#         for j in range(len(strings)):  # 0--2
+#             if l[j][n] == l2[i]:
+#                 answer.append(''.join(l[j]))
+#                 if answer.index(''.join(l[j])):
+#                     del answer[-1]
+#                 else: continue
+#                 print(answer)
+#     return answer
+
+def solution(strings, n):
+    answer = []
+    return answer
+
+
+string2 = ["abce", "abcd", "cdx"]
+n2 = 2
+print(solution(string2, n2))

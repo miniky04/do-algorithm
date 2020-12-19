93176f84a8a90c2661e3d1ab7296c35101d93f5f
minik <miniky04@naver.com>
2020-12-19 23:55:45 +0900

    12943 풀었음

--- programmers/practice1/p12943.py
+++ programmers/practice1/p12943.py
@@ -3,14 +3,18 @@ def solution(num):
     a = True
     while a:
         if num > 1:
+            # 500이 넘으면 -1 반환
             if answer > 500:
                 return -1
+            # 짝수일 때
             elif num % 2 == 0:
                 num = num / 2
                 answer += 1
+            # 홀수일 때
             elif num % 2 == 1:
                 num = num * 3 + 1
                 answer += 1
+        # 1이 된다면 while구문 그만 반복하고 answer값 반환
         elif num == 1:
             a = False
             return answer

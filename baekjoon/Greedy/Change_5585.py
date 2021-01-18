828c611879f5cdf233464df7a58be70afa08166f
minik <miniky04@naver.com>
2021-01-18 12:45:41 +0900

    백준 브론즈2 거스름돈 풀이

---
+++ baekjoon/Greedy/Change_5585.py
@@ -0,0 +1,12 @@
+# total_sum = 1000
+# use = int(input())
+# change = total_sum - use
+change = 1000 - int(input())
+count = 0
+money_list = [500, 100, 50, 10, 5, 1]
+
+for money in money_list:
+    count += change // money
+    change %= money
+
+print(count)

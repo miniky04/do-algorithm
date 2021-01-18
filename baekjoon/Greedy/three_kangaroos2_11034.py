22492a64cac72b38d90920fa09a7e006b4f76e56
minik <miniky04@naver.com>
2021-01-18 12:14:27 +0900

    백준 브론즈3 캥거루 세마리2 풀이 변경

--- baekjoon/Greedy/three_kangaroos2_11034.py
+++ baekjoon/Greedy/three_kangaroos2_11034.py
@@ -10,26 +10,3 @@ while True:
         print(movement_count)
     except EOFError:
         break
-# if one < three:  # 양수일 때
-#     two_plus = two + 1
-#     if two_plus < three:
-#         movement_count += 1
-#         print(movement_count)
-#     else:
-#         print(movement_count)
-# elif one > three:  # 음수일 때
-#     two_plus2 = two + 1
-#     if two_plus2 < one:
-#         movement_count += 1
-#         print(movement_count)
-#     else:
-#         print(movement_count)
-
-# if two - one >= 1:
-#     movement_count = (two - one) - 1
-#     if three - two > 1:
-#         if movement_count < three - two:
-#             movement_count = (three - two) - 1
-#             print(movement_count)
-# else:
-#     print(movement_count)

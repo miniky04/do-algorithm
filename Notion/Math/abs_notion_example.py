3fc54a32b37dbda1aa616f298a61ba7161d3315a
miniky04 <miniky04@naver.com>
2021-02-25 06:09:55 +0900

    Python Math notions upload

---
+++ Notion/Math/abs_notion_example.py
@@ -0,0 +1,7 @@
+abs1 = abs(-3)
+abs2 = abs(-5.72)
+abs3 = abs(3 + 4j)
+
+print(abs1)  # 3
+print(abs2)  # 5.72
+print(abs3)  # 5.0
---
+++ Notion/Math/round_notion_example.py
@@ -0,0 +1,9 @@
+ret1 = round(1118)
+ret2 = round(16.554)
+ret3 = round(1118, -1)  # 1자리에서 반올림해줌
+ret4 = round(16.554, 2)  # 소수점 셋째자리에서 반올림
+
+print(ret1)  # 1118
+print(ret2)  # 17
+print(ret3)  # 1120
+print(ret4)  # 16.55
